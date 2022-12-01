47bf89dc4e8813b73888cf47459d4c760c9bb79f
Luka France <luka.france@sage.com>
2022-12-01 11:14:14 +0000

    updated try/catch fully working

--- remove-logs.ps1
+++ remove-logs.ps1
@@ -1,45 +1,9 @@
-# $path = "..\..\logs-test" 
-# $days = (Get-Date).AddDays(-14)
-# Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} | Remove-Item
-# (Get-Item "C:\Users\luka.france\Desktop\logs-test\old-test-file1.txt").CreationTime=("11 August 2021 15:00:00")
-# (Get-Item "C:\Users\luka.france\Desktop\logs-test\old-test-file.txt").CreationTime=("11 August 2021 15:00:00")
 
+New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old.txt" -ItemType "file" -Value "This is a text string."
+New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old1.txt" -ItemType "file" -Value "This is a text string."
 
-
-# $path = "..\..\logs-test" 
-# try {
-#     Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} | Remove-Item 
-# }
-# catch {
-#     Write-Host "An error has occured"
-# }
-
-
-
-# $path = "..\..\logs-test" 
-
-# try {
-#     Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} | Remove-Item 
-# }
-# catch {
-#     Write-Host "An error has occured"
-# }
-
-
-
-# $path = "..\..\logs-test" 
-# $days = (Get-Date).AddDays(-14)
-# Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} | Remove-Item
-
-
-
-# $path = "..\..\logs-test" 
-# $days = (Get-Date).AddDays(-14) 
-# Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} | Remove-Item 
-# Write-Host "Files older than: $days days will be deleted)"
-
-
-
+(Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old.txt").CreationTime=("11 August 2021 15:00:00")
+(Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old1.txt").CreationTime=("11 August 2021 15:00:00")
 
 $path = "..\..\logs-test" 
 $days = (Get-Date).AddDays(-14) 
@@ -53,7 +17,10 @@ $filesToDelete = Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object 
         $CountNotDeleted++
     }
 
-    Write-Host "Deleted files: $($CountDeleted)"
+    
 
-    "Result : {0} file(s) deleted, {1} file(s) cant removed" -f $CountDeleted, $CountNotDeleted
+
+
+
+
 
