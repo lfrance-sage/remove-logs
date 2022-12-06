e586a74a116953ee4d2959a454f1f64699faa11c
Luka France <luka.france@sage.com>
2022-12-06 14:01:15 +0000

    added log file with date and time

--- remove-logs.ps1
+++ remove-logs.ps1
@@ -1,13 +1,23 @@
-# New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old.txt" -ItemType "file" -Value "This is a text string."
-# New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old1.txt" -ItemType "file" -Value "This is a text string."
-# New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old2.txt" -ItemType "file" -Value "This is a text string."
+New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old.txt" -ItemType "file" -Value "This is a text string."
+New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old1.txt" -ItemType "file" -Value "This is a text string."
+New-Item -Path "C:\Users\luka.france\Desktop\logs-test" -Name "testfile-old2.txt" -ItemType "file" -Value "This is a text string."
 
-# (Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old.txt").CreationTime=("11 August 2021 15:00:00")
-# (Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old1.txt").CreationTime=("11 August 2021 15:00:00")
-# (Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old2.txt").CreationTime=("11 August 2021 15:00:00")
+(Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old.txt").CreationTime=("11 August 2021 15:00:00")
+(Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old1.txt").CreationTime=("11 August 2021 15:00:00")
+(Get-Item "C:\Users\luka.france\Desktop\logs-test\testfile-old2.txt").CreationTime=("11 August 2021 15:00:00")
+
+
+
+
 
 
 function deleteLogs {    
+    
+$logFile = "C:\Users\luka.france\Desktop\my-log-output\log-$(Get-Date -Format yyyy-MM-dd-HH-mm-ss).txt"
+    if (-not (Test-Path $logFile)) {
+        New-Item -Path $logFile -ItemType File
+        Add-Content -Path $logFile -Value "Script started at $(Get-Date)"
+      }
 $path = "..\..\logs-test"    #This is where we will set the path for the log files
 $days = (Get-Date).AddDays(-14)  # The number -14 is the time you want the files to delete after
 $filesToDelete = Get-ChildItem $path -Recurse -Include ("*.txt") | Where-Object {$_.CreationTime -lt $days} 
@@ -28,7 +38,8 @@ foreach ($path in $path ) {
         }
             Write-Host "Succesfully deleted: $($filesToDelete.Count) Files" -ForegroundColor Green
 }
+Add-Content -Path $logFile -Value "Script finished at $(Get-Date)"
 }
-            
-        deleteLogs  #Calls the function to delete the files.
- 
+           
+
+deleteLogs
